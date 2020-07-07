a847d49a956f584d10d48e014db254ac482d698e
Mathias5g <capmathiasdatp@gmail.com>
2020-07-07 18:54:57 -0300

    Correções no HTML e CSS

--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Styles-->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+    <link rel="stylesheet" href="{{asset('css/global.css')}}">
+    <link rel="stylesheet" href=@yield('csspersonalizado')>
     <title>@yield('titulo')</title>
 </head>
 <body>
--- resources/views/qg/login.blade.php
+++ resources/views/qg/login.blade.php
@@ -1,10 +1,20 @@
 @extends('layout.login')
+@section('csspersonalizado', 'css/login.css')
 @section('title', 'Login - QG')
 @section('content')
-    <form action="#" class="form-login">
-        <div class="two-inputs">
-            <input type="text">
-            <input type="text">
-        </div>
-    </form>
+    <div class="form-wrap">
+        <form action="#" class="form-login">
+            <img src="{{asset('img/logos/arma3.png')}}" alt="Logo">
+            <div class="two-inputs">
+                <input type="text" placeholder="Usuário" value="Admin">
+                <input type="password" placeholder="Senha" value="123">
+            </div>
+            <button class="btn btn-blue font-bold">ENTRAR</button>
+            <div class="checkbox-wrap">
+                <input class="checkbox-rounded" type="checkbox" name="checkbox">
+                <label for="checkbox">Lembrar de mim</label>
+            </div>
+        </form>
+    </div>
 @endsection
+
