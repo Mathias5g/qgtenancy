efa15e6418100a2ed412b450e543227c9a9631a8
Mathias5g <capmathiasdatp@gmail.com>
2020-07-16 19:56:39 -0300

    Correção da index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,30 +1,22 @@
-@extends('layout.site')
-@section('title', 'Home - QG')
-@section('csspersonalizado', 'css/missoes.css')
-@section('content')
-    <h3>Lista de missões</h3>
-    <h2>Nenhuma missão para exibir</h2>
-    <table class="table-missions">
-        <thead>
-        <tr>
-            <th>Nome</th>
-            <th>Descrição</th>
-            <th>Tipo</th>
-            <th>Data</th>
-            <th>Status</th>
-            <th>Ações</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th>Jogatina Oficial</th>
-            <th>Devemos localizar os inimigos</th>
-            <th>Oficial</th>
-            <th>09/07/2020 - 20:00</th>
-            <th>Não jogada</th>
-            <th>Editar</th>
-        </tr>
-        </tbody>
-    </table>
-    <button>Cadastrar Missão</button>
-@endsection
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Styles-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/12422af1ba.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+    <link rel="stylesheet" href="{{asset('css/global.css')}}">
+    @yield('cssheader')
+    @yield('scriptsheader')
+    <title>@yield('titulo')</title>
+</head>
+<body>
+<h1>Index</h1>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+@yield('scriptsfooter')
+</body>
+</html>
