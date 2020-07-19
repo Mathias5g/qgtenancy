84e603d3849a3f0e87fe65429660cbf3a67f6726
Mathias5g <capmathiasdatp@gmail.com>
2020-07-19 02:00:19 -0300

    Inicio dos slots

--- resources/views/missoes/_includes/form.blade.php
+++ resources/views/missoes/_includes/form.blade.php
@@ -2,10 +2,8 @@
 <input type="file" name="image" accept="image/*">
 <textarea name="description" id="trumbowyg-demo"></textarea>
 <div class="slots">
-    <div class="teams">
-        <a href="" id="new-team"><button>Nova Equipe</button></a>
-    </div>
-    <div class="members">
-        <a href="" id="new-member"><button>Novo Membro</button></a>
+    <div class="title-slots">
+        <h2>Equipes e Membros</h2>
     </div>
 </div>
+<button type="button" id="addGroup">Adicionar Grupo</button>
--- resources/views/missoes/adicionar.blade.php
+++ resources/views/missoes/adicionar.blade.php
@@ -15,9 +15,31 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.21.0/trumbowyg.min.js"></script>
     <script>
         $('#trumbowyg-demo').trumbowyg();
-        $('#new-team').click(() => {
-            alert('ok')
-        })
+
+        var count = 0;
+        addGroupSlots(count);
+        function addGroupSlots(number) {
+            var trSlots = `<div class="tr-slots" id="group-${count}"></div>`;
+            var trGroup = '<div class="body-slots-groups"><h3>Grupo</h3><div class="div-group"><i class="fa fa-minus" aria-hidden="true"></i><input type="text" name="group[]" /></div></>';
+            var trMember = `<div class="body-slots-members" id="slot-${count}"><h3>Membros</h3><div class="div-member"><input type="text" name="member[]"/><i class="fa fa-minus" aria-hidden="true"></i></div><span></span></div>`;
+            var trMemberSlots = '<button type="button" id="addSlot"><i class="fa fa-plus" aria-hidden="true"></i>  Add slot</button>';
+
+            $('.slots').append(trSlots);
+            $(`#group-${count}`).append(trGroup);
+            $(`#group-${count}`).append(trMember);
+            $(`#slot-${count}`).append(trMemberSlots);
+        }
+
+        $('#addGroup').click(function() {
+            addGroupSlots(count);
+            count++;
+        });
+
+        $('#addSlot').click(function() {
+            var trNewMember = `<div class="div-member"><input type="text" name="member[]"/><i class="fa fa-minus" aria-hidden="true"></i></div><span></span>`;
+            $(`#addSlot`).before(trNewMember);
+            console.log(count)
+        });
     </script>
 @endsection
 
