5375230a4908ee4335dffb6c7d9142433f3822a1
Mathias5g <capmathiasdatp@gmail.com>
2020-08-17 00:03:46 -0300

    alterações com vue

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,7 +16,7 @@
             <li><a href="#tabs-1">Membros</a></li>
             <li><a href="#tabs-2">Configurações do Grupo</a></li>
             <div id="tabs-1">
-                <input type="text" required>
+                <div id="app"></div>
             </div>
             <div id="tabs-2">
                 <input type="text" required>
@@ -28,4 +28,27 @@
     </div>
 @endsection
 @section('scriptsfooter')
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script>
+    new Vue({
+        el: '#app',
+        template: `
+            <h1>Todos os usuários</h1>
+            <div v-for="user in users">
+                <p></p>
+                <button>Delete</button>
+                <button>Editar</button>
+            </div>
+        `,
+        data() {
+            return {
+                users: [
+                {
+                    name: 'Leandro'
+                }
+                ],
+            }
+        }
+    })
+</script>
 @endsection
